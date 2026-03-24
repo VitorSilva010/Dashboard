bddfb78f8f37581a7cc46c3c71fb275045a992f9
Theodoro <jhoonpedro4@gmail.com>
2026-03-23 22:55:25 -0300

    Refeito tud

--- cad-usuarios.php
+++ cad-usuarios.php
@@ -1,67 +1,67 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title></title>
+    <link rel="stylesheet" href="estilo.css">
+    <link rel="stylesheet" href="usuario.css">
+    <link rel="stylesheet" href="dash.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
+
 <body>
-    <main>
-<section class="card-form">
-  <div class="form-header">
-    <h2><i class="fa fa-user-plus"></i> Novo Usuário</h2>
-    <p>Preencha os dados abaixo para registrar um novo acesso.</p>
-  </div>
+    <div class="dashboard-container">
+        <?php include 'menu.php'; ?>
+        <main>
+            <section class="card-form">
+                <div class="form-header">
+                    <h2><i class="fa-solid fa-user-plus"></i> Novo Usuário</h2>
+                    <p>Preencha os dados abaixo para registrar um novo acesso.</p>
+                </div>
 
-  <form action="processa.php" method="POST" class="form">
-    <div class="form-group">
-      <label for="nome">Nome Completo</label>
-      <input 
-        type="text" 
-        id="nome" 
-        name="nome" 
-        placeholder="Ex: Eric Freitas" 
-        required
-      >
+                <form action="processar.php" method="POST">
+                    <div class="form-group">
+                        <label for="nome">Nome completo</label>
+                        <input type="text" id="nome" name="nome" placeholder="Ex: Nome Sobrenome" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">E-mail</label>
+                        <input type="email" id="email" name="email" placeholder="nome@empresa.com" required>
+                    </div>
+
+                    <div class="form-row">
+                        <div class="form-group flex-1">
+                            <label for="senha">Senha</label>
+                            <input type="password" id="senha" name="senha" placeholder="Senha" required>
+                        </div>
+
+                        <div class="form-group flex-1">
+                            <label for="nivel">Nivel</label>
+                            <select id="nivel" name="nivel">
+                                <option value="1">Usuario</option>
+                                <option value="2">Administrador</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="form-actions">
+                        <button class="btn-save" type="submit"><i class="fa-solid fa-check"></i> Finalizar
+                            Cadastro</button>
+                        <a class="btn-cancel" href="index.php" role="button">cancelar</a>
+                    </div>
+
+
+                </form>
+            </section>
+        </main>
     </div>
+    <footer>
+        <?php include 'footer.php'; ?>
+    </footer>
 
-    <div class="form-group">
-      <label for="email">E-mail</label>
-      <input 
-        type="email" 
-        id="email" 
-        name="email" 
-        placeholder="nome@empresa.com" 
-        required
-      >
-    </div>
-
-    <div class="form-row">
-      <div class="form-group flex-1">
-        <label for="senha">Senha</label>
-        <input 
-          type="password" 
-          id="senha" 
-          name="senha" 
-          required
-        >
-      </div>
-
-      <div class="form-group flex-1">
-        <label for="nivel">Nível</label>
-        <select id="nivel" name="nivel">
-          <option value="1">Usuário</option>
-          <option value="2">Administrador</option>
-        </select>
-      </div>
-    </div>
-
-    <div class="form-actions">
-      <button type="submit" class="btn-save">Finalizar Cadastro</button>
-      <a href="index.php" class="btn-cancel">Cancelar</a>
-    </div>
-  </form>
-</section>
-    </main>
 </body>
+
 </html>
---
+++ menu.php
@@ -0,0 +1,12 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Inicio</a></li>
+        <li><a href="categorias.php">Categorias</a></li>
+        <li><a href="projetos.php">Projetos</a></li>
+        <li><a href="usuario.php">Usuario</a></li>
+    </ul>
+    <div class="perfil-usuario ">
+        <img src="https://ui-avatars.com/api/?name=V&background=008080&color=qwe" alt="Avatar">
+        <span>Vitor Silva</span>
+    </div>
+</nav>
